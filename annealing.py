000c790a7b78748516aea1e91d58b75b9f0935ea
tmorealdeb <theophile.moreal-de-brevans@insa-lyon.fr>
2020-01-29 17:14:08 +0100

    ann

---
+++ annealing.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 11:34:56 2020
+
+@author: tmorealdeb
+"""
+
+
+import numpy.random as rn
+import numpy as np
+
+interval=[0.,10.]
+
+def random_neighbour(x, fraction=1):
+    """Move a little bit x, from the left or the right."""
+    amplitude = (max(interval) - min(interval)) * fraction / 10
+    delta = (-amplitude/2.) + amplitude * rn.random_sample()
+    return x + delta
+
+def acceptance_probability(cost, new_cost, temperature):
+    if new_cost < cost:
+        # print("    - Acceptance probabilty = 1 as new_cost = {} < cost = {}...".format(new_cost, cost))
+        return 1
+    else:
+        p = np.exp(- (new_cost - cost) / temperature)
+        # print("    - Acceptance probabilty = {:.3g}...".format(p))
+        return p
+    
+def temperature(fraction):
+    """ Example of temperature dicreasing as the process goes on."""
+    return max(0.01, min(1, 1 - fraction))
+
+def annealing(random_start,
+              cost_function,
+              random_neighbour,
+              acceptance,
+              temperature,
+              maxsteps=1000,
+              debug=True):
+    """ Optimize the black-box function 'cost_function' with the simulated annealing algorithm."""
+    state = random_start
+    cost = cost_function(state)
+    states, costs = [state], [cost]
+    for step in range(maxsteps):
+        fraction = step / float(maxsteps)
+        T = temperature(fraction)
+        new_state = [random_neighbour(state[0], fraction),random_neighbour(state[1], fraction)]
+        new_cost = cost_function(new_state)
+        if debug: print("Step #{:>2}/{:>2} : T = {:>4.3g}, state = {:>4.3g}, cost = {:>4.3g}, new_state = {:>4.3g}, new_cost = {:>4.3g} ...".format(step, maxsteps, T, state, cost, new_state, new_cost))
+        if acceptance_probability(cost, new_cost, T) > rn.random():
+            state, cost = new_state, new_cost
+            states.append(state)
+            costs.append(cost)
+            # print("  ==> Accept it!")
+        # else:
+        #    print("  ==> Reject it...")
+    return state, cost_function(state), states, costs
