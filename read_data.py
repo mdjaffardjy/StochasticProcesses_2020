fce3c47ca7447ef5534089f15f4db218117811e2
tmorealdeb <theophile.moreal-de-brevans@insa-lyon.fr>
2020-02-03 14:55:15 +0100

    reorganiser le cod

--- read_data.py
+++ read_data.py
@@ -43,8 +43,7 @@ plt.show()
 #fill out missing data by interpolation
 anomaly_cpt = pd.Series(anomaly)
 anomaly_cpt = anomaly_cpt.interpolate()
-
-#important = verifier si c'est bon ou si y'a mieux, la c'est de l'interpolation lineaire donc wlh
+#sum of squared errors (Augsburg) : 10151.73
 
 
 #Trend - fitting models to the time series :
@@ -60,27 +59,27 @@ plt.show()
 (fit_0, [resid, rank, sv, rcond]) = np.polynomial.polynomial.Polynomial.fit(date,anomaly_cpt,0,full=True)
 plt.plot(date,fit_0(date),color='black')
 
-#linear 
-
+#linear
 (fit_1, [resid1, rank1, sv1, rcond1]) = np.polynomial.polynomial.Polynomial.fit(date,anomaly_cpt,1,full=True)
 plt.plot(date,fit_1(date),color='blue')
-
+#sum of squared residuals (Augsburg)= 9965.16
 
 #quadratic 
 (fit_2, [resid2, rank2, sv2, rcond2]) = np.polynomial.polynomial.Polynomial.fit(date,anomaly_cpt,2,full=True)
 plt.plot(date,anomaly_cpt,linewidth=0.1)
 plt.plot(date,fit_2(date),color='red')
-#sum of squared residuals = 467.1
-
+#sum of squared residuals (Brazil)= 467.1
+#sum of squared residuals (Augsburg)= 9741.23
 
 #cubic polynomial 
 (fit_3, [resid3, rank3, sv3, rcond3]) = np.polynomial.polynomial.Polynomial.fit(date,anomaly_cpt,3,full=True)
 plt.plot(date,fit_3(date),color='green')
 plt.show()
 #sum of squared residuals (Brazil) = 464.11
+#sum of squared residuals (Augsburg)= 9715.18
 
 #Wich regression model fits best?
-#3 is not better than 2
+#3 is not better than 2 so we choose degree 22
 
 #detrending
 est = fit_2(date)
@@ -88,18 +87,22 @@ stoch = anomaly_cpt-est
 yearly_stoch = np.array([stoch[i-60:i+60].mean() for i in range(60,len(stoch)-60)])
 plt.plot(date, stoch, linewidth=0.1)
 plt.plot(date[60:-60],yearly_stoch,color='red')
-
 #detrended mean (Brazil) : 1.48e-16
+#detrended mean (Augsburg) : 1.38e-16
 
 #params, params_covariance = optimize.curve_fit(test_func, x_data, y_data, p0=[2, 2])
 plt.show()
-#input()
+input()
+
+
 #power spectral density
 dt = 1/12
 f, S= SDE.psd(stoch,dt)
 plt.plot(f,S)
 plt.show()
-#input()
+#The PSD shows a peak at frequency 1, suggesting the presence of an annual cyclic variation in the data
+
+
 #auto-correlation
 
 A = SDE.autocorrel(S)
@@ -109,36 +112,57 @@ plt.show()
 
 #comparison between the two = 
 
-
-
 #Stochastic process that models the temperature anomaly
 xt=fit_2(date)
 
-#brownian
-yt=[stoch[0]]
+input("Start annealing")
+#Stochastic part will be simulated using Ornstein-Uhlenbeck process
+#The parameter mu is set at 0 (mean)
+#The parameters theta and sigma are optimized using simulated annealing in such way that the difference between
+#variance of detrended data and variance of stochastic process (averaged over 10 realizations) is minimal.
+def cost_function(x) :
+    variances=[]
+    for j in range(10) :
+        yt3=[0]
+        theta=x[0]
+        mu=0.
+        sigma_ou=x[1]
+    
+        dt=1/12
+        for t in date[1:] :
+            yt3.append(yt3[-1]-dt*theta*(yt3[-1]-mu)+math.sqrt(dt)*sigma_ou*np.random.normal( ))
+        
+        yt3 = np.array(yt3)
+        #f_sim, S_sim= SDE.psd(yt3,dt)
+        variances.append(yt3.var())
+    variances=np.array(variances)
+    return abs(stoch.var()-variances.mean())
+    #return sum((np.real(S)-np.real(S_sim))**2)
+    #return abs(f[S==max(S)]-f_sim[S_sim==max(S_sim)])[0]
 
-#langevin
-yt2=[0]
-sigma_l=.1
+state, c, states, costs = annealing.annealing([5., 5.],
+              cost_function,
+              annealing.random_neighbour,
+              annealing.acceptance_probability,
+              annealing.temperature,
+              maxsteps=10000,
+              debug=False)
+
+print("theta : "+str(state[0]))
+print("sigma : "+str(state[1]))
 
 #ornstein-uhlenbeck
-yt3=[0]
-theta=3.0240619860568625
+#Values for theta and mu are set after optimization by simulated annealing (see above)
+yt3=[stoch[0]]
+theta=state[0]
 mu=0.
-sigma_ou=3.9956140610085646
+sigma_ou=state[1]
 
 dt=1/12
 for t in date[1:] :
-    #yt.append(yt[-1]+math.sqrt(dt)*np.random.normal(0, math.sqrt(stoch.var())))
-    #yt2.append(yt2[-1]+((yt2[-1]-yt2[-1]**3)+math.sqrt(dt)*sigma_l*np.random.normal(  )))
     yt3.append(yt3[-1]-dt*theta*(yt3[-1]-mu)+math.sqrt(dt)*sigma_ou*np.random.normal( ))
 
-#yt = np.random.normal(0, math.sqrt(stoch.var()), len(date))
-#sim = xt + yt
-#sim2 = xt + yt2
 sim3 = xt + yt3
-#yt = np.array(yt)
-#yt2 = np.array(yt2)
 yt3 = np.array(yt3)
 
 
@@ -150,6 +174,8 @@ plt.plot(date, sim3, linewidth=0.1, color='green')
 plt.plot(date[60:-60], yearly_anomaly, color='red')
 plt.plot(date[60:-60], yearly_sim, color='green')
 plt.show()
+print("yt3 : "+str(yt3.var()))
+print("stoch : "+str(stoch.var()))
 input()
 
 dt = 1/12
@@ -157,28 +183,4 @@ f_sim, S_sim= SDE.psd(yt3,dt)
 A_sim = SDE.autocorrel(S_sim)
 plt.plot(f_sim,S_sim)
 plt.show()
-input("Start annealing")
 
-def cost_function(x) :
-    yt3=[0]
-    theta=x[0]
-    mu=0.
-    sigma_ou=x[1]
-
-    dt=1/12
-    for t in date[1:] :
-        yt3.append(yt3[-1]-dt*theta*(yt3[-1]-mu)+math.sqrt(dt)*sigma_ou*np.random.normal( ))
-    
-    yt3 = np.array(yt3)
-    f_sim, S_sim= SDE.psd(yt3,dt)
-   # return abs(stoch.var()-yt3.var())
-    return sum((np.real(S)-np.real(S_sim))**2)
-
-
-state, c, states, costs = annealing.annealing([5., 5.],
-              cost_function,
-              annealing.random_neighbour,
-              annealing.acceptance_probability,
-              annealing.temperature,
-              maxsteps=10000,
-              debug=False)
